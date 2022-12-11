6b6d442866c2f3baa21f7b53cfdc17fcbd0c53c6
i6nake9 <fleshartemka@gmail.com>
2022-12-11 12:54:02 +0200

    add user model

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
-
+const Product = require('./models/product');
+const User = require('./models/user');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -22,6 +23,9 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+
 sequelize
 	.sync()
 	.then((result) => {
